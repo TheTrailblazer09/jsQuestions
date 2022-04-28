b187a672be94e04bfe5bb20a0d48a9eaec2f4f01
Vani Gupta <59418361+TheTrailblazer09@users.noreply.github.com>
2022-04-28 11:24:45 -0400

    added the question as a comment

--- ithinkwecoulddoitifwetried.js
+++ ithinkwecoulddoitifwetried.js
@@ -1,3 +1,6 @@
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum. 
+//A subarray is a contiguous part of an array.
+
 function largestSum(a){
   let max=a[0];
   let currMax=a[0];
